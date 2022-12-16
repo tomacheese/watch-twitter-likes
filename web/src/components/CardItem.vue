f41f57307e2d78f6043f71c95f978ae84639cb19
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-12-17 05:18:32 +0900

    refactor: 関数定義方法の統一化、流れを追いやすいコード構成への変更

--- web/src/components/CardItem.vue
+++ web/src/components/CardItem.vue
@@ -1,10 +1,15 @@
-<!--suppress TypeScriptCheckImport -->
 <script setup lang="ts">
 import { Buffer } from 'buffer'
-import { PropType, onMounted, ref, Ref } from 'vue'
+import { PropType, onMounted, ref } from 'vue'
 import { useFetch } from '#app'
 import { getColor, Palette } from 'color-thief-node'
 import { Item } from '@/types/types'
+
+// data
+const dataUrl = ref<string>()
+const palette = ref<Palette>()
+const gradient = ref<string>()
+const cardTitleClass = ref<string>()
 
 // props
 const props = defineProps({
@@ -17,9 +22,6 @@ const props = defineProps({
     required: true
   }
 })
-
-const gradient: Ref<string|undefined> = ref(undefined)
-const textColor: Ref<string|undefined> = ref(undefined)
 
 // methods
 const calcHeight = (item: Item) => {
@@ -37,49 +39,7 @@ const getImageUrl = (item: Item) => {
   return `https://pbs.twimg.com/media/${imageId}?format=jpg&name=small`
 }
 
-const getTargetDisplay = (item: Item) => {
-  if (props.isAnd || !item.target) {
-    return ''
-  }
-  return `by ${item.target.name} likes`
-}
-
-const getGradient = async (item: Item) => {
-  const image = new Image()
-  const imageURL = getImageUrl(item)
-  const response = await useFetch(imageURL, { responseType: 'arrayBuffer' })
-  if (!response.data) {
-    throw new Error('response.data is undefined')
-  }
-  const a = response.data.value
-  if (!a) {
-    throw new Error('a is undefined')
-  }
-  // get data url
-  const dataURL = `data:image/jpeg;base64,${Buffer.from(
-      a as any,
-      'binary'
-  ).toString('base64')}`
-  image.onload = async () => {
-    const dominantColor = await getColor(image)
-    console.log('dominantColor', dominantColor)
-    if (rgb2hsl(dominantColor) <= 0.6) {
-      textColor.value = 'text-white text-right text-subtitle-2'
-    } else {
-      textColor.value = 'text-black text-right text-subtitle-2'
-    }
-    gradient.value = `to bottom, rgba(${dominantColor}, .1), rgba(${dominantColor}, .5)`
-  }
-  console.log('dataURL', dataURL)
-  image.src = dataURL
-}
-
-// mounted
-onMounted(() => {
-  getGradient(props.item)
-})
-
-function rgb2hsl(rgb: Palette): number {
+const rgb2Lightness = (rgb: Palette): number => {
   const r = rgb[0] / 255
   const g = rgb[1] / 255
   const b = rgb[2] / 255
@@ -89,17 +49,84 @@ function rgb2hsl(rgb: Palette): number {
 
   return (max + min) / 2
 }
+
+const getTargetDisplay = (item: Item) => {
+  if (props.isAnd || !item.target) {
+    return ''
+  }
+  return `by ${item.target.name} likes`
+}
+
+const isArrayBuffer = (value: any): value is ArrayBuffer => {
+  return value instanceof ArrayBuffer
+}
+
+const getDataUrl = async (url: string) => {
+  const response = await useFetch<ArrayBuffer>(url, { responseType: 'arrayBuffer' })
+  if (!response.data) {
+    throw new Error('response.data is undefined')
+  }
+  const data = response.data.value
+  if (!data) {
+    throw new Error('data is undefined')
+  }
+  if (!isArrayBuffer(data)) {
+    throw new Error('data is not ArrayBuffer')
+  }
+  // get data url
+  const base64 = Buffer.from(data).toString('base64')
+  const dataURL = `data:image/jpeg;base64,${base64}`
+  return dataURL
+}
+
+const getPalette = async (dataUrl: string) => {
+  const image = new Image()
+  return await new Promise<Palette>((resolve, reject) => {
+    image.onload = () => {
+      resolve(getColor(image))
+    }
+    image.onerror = (error) => {
+      reject(error)
+    }
+    image.src = dataUrl
+  })
+}
+
+const getCardTitleClass = (palette: Palette) => {
+  const commonClasses = 'text-right text-subtitle-2'
+  const lightness = rgb2Lightness(palette)
+  // 0.6以上なら白、0.6未満なら黒
+  return lightness >= 0.6 ? `${commonClasses} text-white` : `${commonClasses} text-black`
+}
+
+const getGradient = (palette: Palette) => {
+  // Palette.toString() は、"r, g, b" の形式っぽい
+  return `to bottom, rgba(${palette}, .1), rgba(${palette}, .5)`
+}
+
+onMounted(async () => {
+  // ツイートの画像 URL
+  const imageURL = getImageUrl(props.item)
+  // 画像を Data URL に変換
+  dataUrl.value = await getDataUrl(imageURL)
+  // パレットを取得
+  palette.value = await getPalette(dataUrl.value)
+  // カードタイトルのクラスを作成
+  cardTitleClass.value = getCardTitleClass(palette.value)
+  // gradientを作成
+  gradient.value = getGradient(palette.value)
+})
 </script>
 
 <template>
   <v-card width="240px">
     <v-img
       :height="calcHeight(item)"
-      :src="getImageUrl(item)"
+      :src="dataUrl"
       class="align-end"
       :gradient="gradient"
     >
-      <v-card-title :class="textColor">
+      <v-card-title :class="cardTitleClass">
         {{ getTargetDisplay(item) }}
       </v-card-title>
       <template #placeholder>
