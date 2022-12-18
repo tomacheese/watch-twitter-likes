f4407ce8db51de217d53d474cb3d35c0e95c2fa8
Tomachi <8929706+book000@users.noreply.github.com>
2022-12-18 14:24:23 +0900

    fix: マイグレーションが無限に終わらない問題の修正 (#98)

--- crawler/src/migration.ts
+++ crawler/src/migration.ts
@@ -3,23 +3,36 @@ import { IsNull } from 'typeorm'
 import { DBTweet } from './entities/tweets'
 
 export async function migrateTweetHashTags(twitterApi: TwitterApi) {
-  let tweetList = await DBTweet.find({
+  const tweets = await DBTweet.find({
     where: {
       tags: IsNull(),
     },
   })
-  console.log('migration tweets: ' + tweetList.length)
+  const checkTweetIds = []
+  for (const tweet of tweets) {
+    const tags = tweet.tags
+    if (tags === null) {
+      continue
+    }
+    checkTweetIds.push(tweet.tweetId)
+  }
 
+  if (checkTweetIds.length === 0) {
+    return
+  }
+  console.log('migration tweets: ' + checkTweetIds.length)
+
+  // 100件ずつに分割
+  const chunks: string[][] = checkTweetIds.reduce((acc, cur, i) => {
+    if (i % 100 === 0) {
+      acc.push([])
+    }
+    acc[acc.length - 1].push(cur)
+    return acc
+  }, [] as string[][])
   let count = 0
-  while (tweetList.length > 0) {
-    tweetList = await DBTweet.find({
-      where: {
-        tags: IsNull(),
-      },
-      take: 100,
-    })
-    const tweetIds = tweetList.map((tweet) => tweet.tweetId)
-    const tweets = await twitterApi.v1.tweets(tweetIds, {
+  for (const chunk of chunks) {
+    const tweets = await twitterApi.v1.tweets(chunk, {
       include_entities: true,
     })
     for (const tweet of tweets) {
