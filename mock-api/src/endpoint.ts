931550d6026e5dbd6402128c730ec5ba13d08555
Tomachi <8929706+book000@users.noreply.github.com>
2022-12-28 01:12:53 +0900

    feat: mock-apiでプロダクションAPIを利用するように (#128)

--- mock-api/src/endpoint.ts
+++ mock-api/src/endpoint.ts
@@ -1,66 +1,6 @@
 import { FastifyRequest, FastifyReply, FastifyInstance } from 'fastify'
 import axios from 'axios'
 
-interface ImageItem {
-  rowId: number
-  createdAt: string
-  tweet: {
-    tweetId: string
-    text: string
-    tags: string[]
-    user: {
-      userId: string
-      screenName: string
-      createdAt: string
-    }
-  }
-  images: {
-    rowId: number
-    imageId: string
-    size: string
-    width: number
-    height: number
-    createdAt: string
-    url: string
-  }[]
-  target: {
-    userId: string
-    name: string
-    threadId: string | null
-    createdAt: string
-  }
-}
-
-interface PicsumImage {
-  id: string
-  author: string
-  width: number
-  height: number
-  url: string
-  download_url: string
-}
-
-const targets = [
-  {
-    userId: '1',
-    name: 'John Smith',
-    threadId: null,
-    createdAt: '2021-01-01T00:00:00.000Z',
-  },
-  {
-    userId: '2',
-    name: 'Jane Smith',
-    threadId: null,
-    createdAt: '2021-01-02T00:00:00.000Z',
-  },
-  {
-    userId: '3',
-    name: 'John Doe',
-    threadId: null,
-    createdAt: '2021-01-03T00:00:00.000Z',
-  },
-]
-
 export class EndPoints {
   private fastify: FastifyInstance
 
@@ -83,56 +23,29 @@ export class EndPoints {
     _request: FastifyRequest,
     reply: FastifyReply
   ): Promise<void> {
-    reply.send(targets)
+    const response = await axios.get('https://likes.amatama.net/api/targets')
+    if (response.status !== 200) {
+      reply.code(500)
+      reply.send({ error: 'Internal Server Error' })
+      return
+    }
+    reply.send(response.data)
   }
 
   async routeGetImages(
-    _request: FastifyRequest,
+    request: FastifyRequest,
     reply: FastifyReply
   ): Promise<void> {
-    const dummyImages = await axios.get<PicsumImage[]>(
-      'https://picsum.photos/v2/list?limit=100'
-    )
+    const queries = request.query
 
-    const randomImages = dummyImages.data.sort(() => Math.random() - 0.5)
-
-    const response: ImageItem[] = []
-    let num = 1
-    for (const image of randomImages) {
-      const dummyTweet: ImageItem['tweet'] = {
-        tweetId: Math.floor(Math.random() * 10000000000000).toString(),
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        tags: ['dummy', 'tweet'],
-        user: {
-          userId: Math.floor(Math.random() * 10000000000000).toString(),
-          screenName: 'Lorem Ipsum',
-          createdAt: '2021-01-01T00:00:00.000Z',
-        },
-      }
-
-      const dummyTarget: ImageItem['target'] =
-        targets[Math.floor(Math.random() * targets.length)]
-
-      response.push({
-        rowId: num,
-        createdAt: '2021-01-01T00:00:00.000Z',
-        tweet: dummyTweet,
-        images: [
-          {
-            rowId: Math.floor(Math.random() * 10000000000000),
-            imageId: image.id,
-            size: 'small',
-            width: image.width,
-            height: image.height,
-            createdAt: '2021-01-01T00:00:00.000Z',
-            url: image.download_url,
-          },
-        ],
-        target: dummyTarget,
-      })
-      num++
+    const response = await axios.get('https://likes.amatama.net/api/images', {
+      params: queries,
+    })
+    if (response.status !== 200) {
+      reply.code(500)
+      reply.send({ error: 'Internal Server Error' })
+      return
     }
-
-    reply.send(response)
+    reply.send(response.data)
   }
 }
