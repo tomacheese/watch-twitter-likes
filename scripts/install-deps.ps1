2dbdb63db886252d8594c9fa44e7449b7dd5931d
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-12-27 21:41:22 +0900

    fix: yarnコマンドの実行方法をコマンドプロンプト経由に変更

--- scripts/install-deps.ps1
+++ scripts/install-deps.ps1
@@ -2,9 +2,9 @@ Set-Location $PSScriptRoot
 Set-Location ..
 
 Get-ChildItem */node_modules | Remove-Item -Force -Recurse -ErrorAction Stop
-Start-Process -FilePath yarn -ArgumentList "install" -WorkingDirectory "crawler" -NoNewWindow -Wait
-Start-Process -FilePath yarn -ArgumentList "install" -WorkingDirectory "mock-api" -NoNewWindow -Wait
-Start-Process -FilePath yarn -ArgumentList "install" -WorkingDirectory "web" -NoNewWindow -Wait
+Start-Process -FilePath cmd -ArgumentList "/C yarn install" -WorkingDirectory "crawler" -NoNewWindow -Wait
+Start-Process -FilePath cmd -ArgumentList "/C yarn install" -WorkingDirectory "mock-api" -NoNewWindow -Wait
+Start-Process -FilePath cmd -ArgumentList "/C yarn install" -WorkingDirectory "web" -NoNewWindow -Wait
 
 Write-Host
 Write-Host Please press any key... -NoNewLine
